3c0fe7b28746aaebd3d31f69161a40d1b0232ce2
Haley Zeng <hzeng@stuy.edu>
2017-02-04 00:33:16 -0500

    update gitignore

--- firstimage.py
+++ firstimage.py
@@ -8,16 +8,7 @@ pic = open("pic.ppm", "w")
 pic.write("P3 %d %d 255\n" % (xres, yres))
 for i in range(xres):
     for j in range(yres):
-        iEven = i % 2 == 0
-        jEven = j % 2 == 0
-        if iEven and jEven:
-            red = 255
-        else if iEven and  not jEven:
-            red = 100
-        else if not iEven and jEven:
-            red = 255
-        else:
-            red = 100
+        red = 100
         green = 255
         blue = 255     
         pic.write("%d %d %d\n" % (red, green, blue))
