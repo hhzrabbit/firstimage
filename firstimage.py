8eb5833e89d80ed3c03ce9b40f0c037cfbaddbee
Haley Zeng <hzeng@stuy.edu>
2017-02-04 00:39:27 -0500

    one circle

--- firstimage.py
+++ firstimage.py
@@ -1,16 +1,30 @@
 import math
 
+def dist(x1, y1, x2, y2):
+    return math.sqrt(((x1-x2) ** 2) + ((y1-y2) ** 2))
+
 xres = 500
 yres = 500
+
+centerx = 250
+centery = 250
 
 pic = open("pic.ppm", "w")
 
 pic.write("P3 %d %d 255\n" % (xres, yres))
 for i in range(xres):
     for j in range(yres):
-        red = 100
-        green = 255
-        blue = 255     
+        d = dist(i, j, centerx, centery)
+        if d % 10 < 5:
+            red = 255
+            green = 0
+            blue = 0
+        else:
+            red = 0
+            green = 0
+            blue = 0
+        
         pic.write("%d %d %d\n" % (red, green, blue))
 
 pic.close()
+
